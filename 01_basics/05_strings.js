a21fd111f49fac28ae6d4e4c65db4222f1229c26
Devesh Baghel <143107270+Devesh-baghel@users.noreply.github.com>
2025-03-31 15:23:41 +0000

    strings in js

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,27 @@
+const name = "devesh"
+const repoCount = 50 
+
+// console.log(name+ repoCount + "Value");
+// console.log(`Hello my n ame is ${name} and my repo count is ${repoCount} `);
+ 
+const gameName= new String('tica-tac');
+
+// console.log(gameName.length);
+
+// console.log(gameName.toUpperCase());
+ 
+
+const newString =gameName.substring(0,4);
+console.log(newString);
+
+const newStringOne = "  hello   ";
+console.log(newStringOne);
+
+console.log(newStringOne.trim()); // remove extra whitespace at start
+
+const url = "https://deveshbaghel.com/devesh%22";
+
+console.log(url.replace('%22','-'));
+
+
+
