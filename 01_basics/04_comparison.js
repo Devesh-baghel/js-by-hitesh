ffc752d4d1268d73dab010ed0c21fa3bbb0a2088
Devesh Baghel <143107270+Devesh-baghel@users.noreply.github.com>
2025-03-31 14:37:05 +0000

    comparisons

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,14 @@
+// console.log(2>1);
+// console.log(2 >=1);
+// console.log( 2==1);
+// console.log(2!=1);
+
+// console.log( null>= 0);   avoid these comparisons
+// console.log(null === 0);
+// console.log(null == 0);
+
+ console.log(undefined==0);
+ 
+ console.log(undefined>=0);
+ 
+ console.log(undefined < 0);
