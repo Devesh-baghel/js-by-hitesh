d56966462dee411e6aa5fe65cdc36ba90fb3651c
Devesh Baghel <143107270+Devesh-baghel@users.noreply.github.com>
2025-03-31 13:35:02 +0000

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict";  // treat all js code as newer version
+
+// alert(3+3) // we are using nodejs ,not browser
+
+console.log(3+3); 
+
+let name ="boss";
+let age = 45;
+ let isLoggedIn =false;
+
+ // number => 2^53
+ //bigInt
+ // string ' ' /  " "
+ //boolean => true/false
+ // null =>standalone value
+ // undefined => value not assigned
+ // symbol 
+ // object 
+
+ console.log(typeof undefined);
