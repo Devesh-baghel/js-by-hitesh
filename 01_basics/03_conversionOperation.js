03bd37b5347b58b83cf77f601d8d78c698f1dd97
Devesh Baghel <143107270+Devesh-baghel@users.noreply.github.com>
2025-03-31 14:27:44 +0000

    conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score ="33abc";
+
+ let valueInNumber = Number(score);
+ let valueInString =String(score);
+//console.log(typeof valueInString);
+let isLoggedIn = 1;
+let inBoolean = Boolean(isLoggedIn);
+console.log(typeof inBoolean);
+
+let number = null; 
+console.log(valueInNumber);
+// "33" => 33
+//"33abc" => NaN
+// true=> 1; false =>0
+
+let someNumber = 33;
+
+let stringNumber =String(someNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+let str1="hello"
+let str2= "bye"
+
+let str3= str1+str2;
+// console.log(str3);
+// console.log("1"+2);   // 12
+// console.log(1+"2");    //12
+// console.log("1"+2+2);    //122
+// console.log(1+2+"2");   // 32
+
+let num1,num2,num3;
