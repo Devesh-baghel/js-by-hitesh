2c21e1c1c2220996ca31bd258fb530972d71e642
Devesh Baghel <143107270+Devesh-baghel@users.noreply.github.com>
2025-04-01 02:57:52 +0000

    dates in js

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,16 @@
+//Dates
+
+let myDate = new Date();
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleString());
+//let myCreatedDate= new Date(2025,3,1);
+// let myCreatedDate= new Date("04-01-2025");
+// console.log(myCreatedDate.toDateString());
+// let myTimeStamp = Date.now();
+// console.log(myTimeStamp);
+console.log(Math.floor(Date.now()/1000));
+
+console.log(myDate.getMonth());
+
+
+
