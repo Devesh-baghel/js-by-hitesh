2aa05154aff3069805887fb0826d0a7475744653
Devesh Baghel <143107270+Devesh-baghel@users.noreply.github.com>
2025-04-01 16:04:15 +0000

    while loops in js

---
+++ 05_iterations/two.js
@@ -0,0 +1,20 @@
+let i = 0;
+while(i<=10){
+    console.log(`value of i is : ${i}`);
+    i++;
+}
+let myArray = ['flash','batman','doraemon'];
+
+let index = 0;
+while(index<myArray.length){
+    console.log(myArray[index]);
+    index++;
+    
+}
+let score =10;
+
+do{
+    console.log(`score is ${score}`);
+    score++;
+    
+}while(score<=15);
