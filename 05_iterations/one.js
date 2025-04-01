a42e71f31b4f810317c4d64d3bd89ec3116b09cf
Devesh Baghel <143107270+Devesh-baghel@users.noreply.github.com>
2025-04-01 15:59:25 +0000

    for loop in js

---
+++ 05_iterations/one.js
@@ -0,0 +1,27 @@
+// for
+const array =  [1,2,3,4,5,6];
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+   // console.log(element);
+    
+    
+}
+
+let myArray = ['flash ','batman','superman'];
+
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+ //   console.log(element);
+    
+}
+//break and continue
+
+for(let index = 0; index<=20;index++){
+
+    if(index==5){
+        //console.log(`detected 5`);
+        continue;
+    }
+   // console.log(`value of i is ${index}`);
+     }
+
