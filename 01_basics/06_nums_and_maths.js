8f41b7143fb7b442dbd67b0a88a3abe1b2b86824
Devesh Baghel <143107270+Devesh-baghel@users.noreply.github.com>
2025-04-01 02:33:15 +0000

    working with maths

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,17 @@
+const score= 400; 
+
+const balance = new Number(100);
+// console.log(balance);
+// console.log(score.toFixed(2));
+// const hundreds = 10100000;
+// console.log(hundreds.toLocaleString('en-IN'));
+// console.log(Math.abs(-1)); // convert negative to positive
+// console.log(Math.round(4.2));
+// console.log(Math.random());
+
+const min =10;
+const max =20;
+
+console.log(Math.floor(Math.random() * (max- min +1))+ min);
+
+
