04a1efe43c5ebee4a1ee1cb6aa745a1499679109
Devesh Baghel <143107270+Devesh-baghel@users.noreply.github.com>
2025-04-01 03:36:56 +0000

    array  in js

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,20 @@
+//
+ const myArray = [0,1,2,3,4,5];
+
+ const heross = ["shaktiman"," superman"];
+
+ // array methods
+ myArray.push(7);
+ //console.log(myArray);
+
+ //slice,splice 
+
+ console.log("A",myArray);
+
+ const myn1= myArray.slice(1,3);
+ console.log(myn1);
+ console.log("B",myArray);
+ 
+ const myn2= myArray.splice(1,3);
+ console.log(myn2);
+ // splice deletes the data from original array
