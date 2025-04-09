b9972c2119ef5390ecf2d781177ad0208c8d909a
Devesh Baghel <143107270+Devesh-baghel@users.noreply.github.com>
2025-04-09 04:57:07 +0000

    static property

---
+++ 10_oop/staticprop.js
@@ -0,0 +1,28 @@
+class User{
+    constructor(username){
+        this.username= username
+    }
+
+    logMe(){
+        console.log(`Username: ${this.username}`);
+        
+    }
+
+  static  createId(){
+        return `123`
+    }
+}
+const devesh = new User('devesh')
+//console.log(devesh.createId());
+
+class Teacher extends User{
+    constructor(username,email){
+        super(username)
+        this.email= email
+    }
+}
+
+const iphone = new Teacher('iphone','i@phone.com')
+iphone.logMe();
+
+
